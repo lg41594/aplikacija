ab314505fc33ec75f445c12858f6054611b22171
lana geric <gerichenator@gmail.com>
2012-07-10 09:02:37 +0200

    prvo spremanje, instalirani svi gem-ovi

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aplikacija::Application.config.secret_token = '8fd976f88dce691f882d8dfd458b12f06f909867fb78bbd9222ef2009349298d1196c6b927774abfc7b11473a28452667fb5e48e6848104537f547b192fc2e3e'
